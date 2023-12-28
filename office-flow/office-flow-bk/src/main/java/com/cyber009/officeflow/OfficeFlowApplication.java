8174e5bb50c5fce0bc0c061300791ed7053cfede
asraful islam <asraful009@gmail.com>
2023-12-28 18:09:55 +0600

    office flow angular

---
+++ office-flow/office-flow-bk/src/main/java/com/cyber009/officeflow/OfficeFlowApplication.java
@@ -0,0 +1,13 @@
+package com.cyber009.officeflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfficeFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfficeFlowApplication.class, args);
+	}
+
+}
---
+++ office-flow/office-flow-bk/src/test/java/com/cyber009/officeflow/OfficeFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cyber009.officeflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfficeFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
