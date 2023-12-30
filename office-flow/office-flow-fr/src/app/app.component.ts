b68488d8045b2061c87d839a2f6e6731f0be9e60
asraful islam <asraful009@gmail.com>
2023-12-30 17:45:20 +0600

    ng

--- office-flow/office-flow-fr/src/app/app.component.ts
+++ office-flow/office-flow-fr/src/app/app.component.ts
@@ -1,14 +1,18 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
-import { MatIconModule } from "@angular/material/icon";
-import { MatGridListModule } from "@angular/material/grid-list";
+import {Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {RouterLink, RouterOutlet} from '@angular/router';
+import {MatGridListModule} from "@angular/material/grid-list";
+import {MatIconModule} from "@angular/material/icon";
+import {MatMenuModule} from "@angular/material/menu";
+import {MatListModule} from "@angular/material/list";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MatIconModule , MatGridListModule ],
+  imports: [CommonModule, RouterOutlet,
+    MatGridListModule, MatIconModule, MatMenuModule, MatListModule, RouterLink
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
