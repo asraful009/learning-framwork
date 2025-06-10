aefe6d14316959ec0f3c23a4fe46555d5949311a
Asraful Islam <asraful009@gmail.com>
2025-06-10 19:28:25 +0600

    end point to user session

--- s3-security-t2/src/main/java/com/cyber009/s3t2/filter/RolePermissionFilter.java
+++ s3-security-t2/src/main/java/com/cyber009/s3t2/filter/RolePermissionFilter.java
@@ -1,5 +1,9 @@
 package com.cyber009.s3t2.filter;
 
+import com.cyber009.s3t2.dto.EndPointDto;
+import com.cyber009.s3t2.entity.UserLoginSessionEntity;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -7,10 +11,13 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
@@ -22,8 +29,30 @@ public class RolePermissionFilter extends OncePerRequestFilter {
                                     @NonNull HttpServletResponse response,
                                     @NonNull FilterChain filterChain) throws ServletException, IOException {
         log.info("RolePermissionFilter: Processing request for URL: {}", request.getRequestURI());
-        var sessionInfo = request.getAttribute("session_info");
+        UserLoginSessionEntity sessionInfo = (UserLoginSessionEntity) request.getAttribute("session_info");
         log.info("RolePermissionFilter: Session info: {}", sessionInfo);
+        if( sessionInfo == null) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        ObjectMapper mapper = new ObjectMapper();
+        List<EndPointDto> endPointDtoList =
+                mapper.readValue(sessionInfo.getRoleJson(), new TypeReference<>() {});
+        boolean isEndpointAllowed = false;
+        if(endPointDtoList != null && !endPointDtoList.isEmpty()) {
+            for (EndPointDto endPoint : endPointDtoList) {
+                if (request.getRequestURI().equals(endPoint.getEndPoint())) {
+                    isEndpointAllowed = true;
+                }
+            }
+        } else {
+            log.warn("RolePermissionFilter: No permissions found for user.");
+        }
+        if(!isEndpointAllowed) {
+            log.warn("RolePermissionFilter: Access denied for endpoint: {}", request.getRequestURI());
+            response.sendError(HttpServletResponse.SC_FORBIDDEN, "Access Denied");
+            return;
+        }
         filterChain.doFilter(request, response);
     }
 }
